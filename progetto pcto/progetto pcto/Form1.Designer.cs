e8138a09957ed780c98b88d1e9d383db08edcd62
alessiabirolinii <birolini.alessia@gmail.com>
2021-02-13 12:45:17 +0100

    agg

--- progetto pcto/progetto pcto/Form1.Designer.cs
+++ progetto pcto/progetto pcto/Form1.Designer.cs
@@ -29,125 +29,186 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
-            this.label3 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(175, 650);
+            this.panel2.TabIndex = 16;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Transparent;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.Location = new System.Drawing.Point(3, 288);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(175, 155);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Mappa";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.Color.SkyBlue;
-            this.button1.Location = new System.Drawing.Point(323, 13);
+            this.button1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Transparent;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.Location = new System.Drawing.Point(0, 104);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(95, 41);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "cerca";
+            this.button1.Size = new System.Drawing.Size(175, 160);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Ricerca Cantanti";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Teal;
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(175, 107);
+            this.panel3.TabIndex = 18;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label1.Location = new System.Drawing.Point(3, 40);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(110, 15);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Che cantante vuoi?";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(141, 24);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(158, 21);
-            this.comboBox1.TabIndex = 3;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 76);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(187, 507);
-            this.dataGridView1.TabIndex = 4;
+            this.label1.Size = new System.Drawing.Size(165, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "save concert";
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(205, 76);
+            this.panel1.CausesValidation = false;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(175, 529);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(570, 507);
-            this.panel1.TabIndex = 5;
+            this.panel1.Size = new System.Drawing.Size(622, 121);
+            this.panel1.TabIndex = 17;
             // 
-            // pictureBox2
+            // button3
             // 
-            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox2.Location = new System.Drawing.Point(762, 24);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(25, 21);
-            this.pictureBox2.TabIndex = 15;
-            this.pictureBox2.TabStop = false;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(656, 69);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 38);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "cantante";
+            this.button3.UseVisualStyleBackColor = true;
             // 
-            // comboBox3
+            // button4
             // 
-            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox3.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(500, 24);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(256, 21);
-            this.comboBox3.TabIndex = 14;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(532, 69);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(109, 38);
+            this.button4.TabIndex = 20;
+            this.button4.Text = "città";
+            this.button4.UseVisualStyleBackColor = true;
             // 
-            // label3
+            // button5
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(442, 26);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(43, 13);
-            this.label3.TabIndex = 13;
-            this.label3.Text = "Color:";
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(396, 69);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(109, 38);
+            this.button5.TabIndex = 21;
+            this.button5.Text = "giorno";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Location = new System.Drawing.Point(268, 69);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(109, 38);
+            this.button6.TabIndex = 22;
+            this.button6.Text = "ora";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(175, 104);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(621, 339);
+            this.listView1.TabIndex = 23;
+            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
             this.ClientSize = new System.Drawing.Size(797, 650);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.comboBox3);
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel2);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "SAVE CONCERT";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.ComboBox comboBox3;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ListView listView1;
     }
 }
 
--- progetto pcto/progetto pcto/Form1.cs
+++ progetto pcto/progetto pcto/Form1.cs
@@ -19,55 +19,63 @@ namespace progetto_pcto
         {
             InitializeComponent();
         }
-        
+        #region
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    int a = 0;
+        //    string b = null;
+        //    ArrayList ColorList = new ArrayList();
+        //    Type colorType = typeof(System.Drawing.Color);
+        //    PropertyInfo[] propInfoList = colorType.GetProperties(BindingFlags.Static |
+        //                                  BindingFlags.DeclaredOnly | BindingFlags.Public);
+        //    foreach (PropertyInfo a in propInfoList)
+        //    {
+        //        //this.comboBox2.Items.Add(a.Name);
+        //    }
+        //    List<string> Elecantanti = new List<string>();
+        //    List<string> Elecanzoni = new List<string>();
+        //    List<string> Elecitta = new List<string>();
+
+        //    if (b == "")
+        //    {
+        //        MessageBox.Show("inserire un cantante");
+        //        return;
+        //    }
+        //    var cantante = a;
+
+        //    #region
+        //    HttpClient clint = new HttpClient();
+        //    clint.BaseAddress = new Uri("https://api.setlist.fm/docs/");
+        //    HttpResponseMessage risposta = clint.GetAsync("1.0/search/artists").Result;
+        //    var artista = risposta.Content.ReadAsStringAsync().Result;
+
+        //    //comboBox1.Text = risposta.ToString();
+        //    //dataGridView1.DataSource = artista;
+
+        //    HttpClient c = new HttpClient();
+        //    clint.BaseAddress = new Uri("https://api.setlist.fm/docs/");
+        //    HttpResponseMessage r = clint.GetAsync("1.0/search/cities").Result;
+        //    var city = risposta.Content.ReadAsStringAsync().Result;
+
+        //    HttpClient cl = new HttpClient();
+        //    clint.BaseAddress = new Uri("https://api.setlist.fm/docs/");
+        //    HttpResponseMessage re = clint.GetAsync("1.0/search/setlists").Result;
+        //    var song = risposta.Content.ReadAsStringAsync().Result;
+
+        //    HttpClient clie = new HttpClient();
+        //    clint.BaseAddress = new Uri("https://api.setlist.fm/docs/");
+        //    HttpResponseMessage resp = clint.GetAsync("1.0/search/{mbid}/setlists").Result;
+        //    var datac = risposta.Content.ReadAsStringAsync().Result;
+   #endregion
+
         private void button1_Click(object sender, EventArgs e)
         {
-            ArrayList ColorList = new ArrayList();
-            Type colorType = typeof(System.Drawing.Color);
-            PropertyInfo[] propInfoList = colorType.GetProperties(BindingFlags.Static |
-                                          BindingFlags.DeclaredOnly | BindingFlags.Public);
-            foreach (PropertyInfo a in propInfoList)
-            {
-                //this.comboBox2.Items.Add(a.Name);
-            }
-            List<string> Elecantanti = new List<string>();
-            List<string> Elecanzoni = new List<string>();
-            List<string> Elecitta = new List<string>();
-
-            if (comboBox1.Text== "")
-            {
-                MessageBox.Show("inserire un cantante");
-                return;
-            }
-            var cantante = comboBox1.Text;
-
-            #region
-            HttpClient clint = new HttpClient();
-            clint.BaseAddress = new Uri("https://api.setlist.fm/docs/");
-            HttpResponseMessage risposta = clint.GetAsync("1.0/search/artists").Result;
-            var artista = risposta.Content.ReadAsStringAsync().Result;  
-
-            //comboBox1.Text = risposta.ToString();
-            dataGridView1.DataSource = artista;
-
-            HttpClient c = new HttpClient();
-            clint.BaseAddress = new Uri("https://api.setlist.fm/docs/");
-            HttpResponseMessage r = clint.GetAsync("1.0/search/cities").Result;
-            var city = risposta.Content.ReadAsStringAsync().Result;
-
-            HttpClient cl = new HttpClient();
-            clint.BaseAddress = new Uri("https://api.setlist.fm/docs/");
-            HttpResponseMessage re = clint.GetAsync("1.0/search/setlists").Result;
-            var song = risposta.Content.ReadAsStringAsync().Result;
-
-            HttpClient clie = new HttpClient();
-            clint.BaseAddress = new Uri("https://api.setlist.fm/docs/");
-            HttpResponseMessage resp = clint.GetAsync("1.0/search/{mbid}/setlists").Result;
-            var datac = risposta.Content.ReadAsStringAsync().Result;
-            #endregion
-
 
         }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
