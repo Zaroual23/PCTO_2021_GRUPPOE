bcec9e0ad13d1ba345779d8a0cedf3f1fd556985
lmSchool <62596308+lmSchool@users.noreply.github.com>
2021-02-14 13:17:02 +0100

    Add files via upload

--- progetto pcto/progetto pcto/GlobeViewer/Classes/GlobeViewer.cs
+++ progetto pcto/progetto pcto/GlobeViewer/Classes/GlobeViewer.cs
@@ -11,69 +11,83 @@ using GlobeViewer.Interfaces;
 
 namespace GlobeViewer.Classes
 {
+    /// <summary>
+    /// "GlobeViewer" application main class
+    /// </summary>
     public class GlobeViewer : IGlobeViewer
     {
         private readonly ChromiumWebBrowser browser = default(ChromiumWebBrowser);
         private readonly IJavascriptIntegrationService javascriptIntegrationService = default(IJavascriptIntegrationService);
-        private readonly IGeocoder geocoder = default(IGeocoder);
         private readonly IHttpServer httpServer = default(IHttpServer);
         private const string publicOpenglobusApplicationUrl = "http://lmschool.altervista.org/SaveConcert";
         private bool disposed;
 
+        /// <summary>
+        /// Initializes the "GlobeViewer" application
+        /// </summary>
+        /// <param name="panel">Panel used as a container for the "ChromiumWebBrowser" CefSharp control</param>
         public GlobeViewer(Panel panel)
         {
 
             string openglobusApplicationUrl = default(string);
 
+
+            //Getting a suitable HTTP server running the "OpenglobusApplication" 
             if (new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator))
             {
+                //If the program is running as Administrator, an HTTP server will be created and will host the "OpenglobusApplication" locally
                 httpServer = new HttpServer();
                 openglobusApplicationUrl = "http://localhost:" + httpServer.Port.ToString();
             }
             else
             {
+                //If the program is not running as Administrator, a remote HTTP server hosting the "OpenglobusApplication" will be used instead
                 openglobusApplicationUrl = publicOpenglobusApplicationUrl;
             }
 
+            //Initializing the browser that will present the "OpenglobusApplication"
             browser = new ChromiumWebBrowser(openglobusApplicationUrl)
             {
                 Dock = DockStyle.Fill,
             };
 
+            //Initializing the Javascript integration service for communication between .NET and Javascript 
             javascriptIntegrationService = new JavascriptIntegrationService(browser);
-
-            geocoder = new Geocoder();
 
 
             panel.Controls.Add(browser);
         }
 
-        public void LoadMarkers(IList<string> locations)
+        /// <summary>
+        /// Load markers on the globe
+        /// </summary>
+        /// <param name="locations">List of tuples each one cointaining name and coordinates for a new marker</param>
+        public void LoadMarkers(IList<(string Name, string X, string Y)> locations)
         {
-            IDictionary<string, Coordinates> geocodedLocations = geocoder.Geocode(locations);
-            LoadMarkers(geocodedLocations);
-        }
-
-        public void LoadMarkers(IDictionary<string, Coordinates> locations)
-        {
+            //Preparing the parameter required by the "loadMarkers" Javascript procedure
             string markersList = "[";
 
             foreach (var location in locations)
             {
                 markersList += "{name: '";
-                markersList += location.Key;
+                markersList += location.Name;
                 markersList += "', latitude: ";
-                markersList += location.Value.Y;
+                markersList += location.Y;
                 markersList += ", longitude: ";
-                markersList += location.Value.X;
+                markersList += location.X;
                 markersList += "}, ";
             }
 
             markersList += "]";
 
+            //Finally call the Javascript procedure
             javascriptIntegrationService.CallProcedure("window.loadMarkers(" + markersList + ")");
         }
 
+        /// <summary>
+        /// Bind event handler to handle a "MarkerClickedEvent"
+        /// </summary>
+        /// <param name="procedure">delegate with a void (object, string) signature</param>
         public void BindMarkerClickedEvent(MarkerClickedEventHandler procedure)
         {
             javascriptIntegrationService.MarkerClicked += procedure;
@@ -93,6 +107,7 @@ namespace GlobeViewer.Classes
                 {
 
                 }
+                //Stopping the HTTP server if not null, so as not to leave running processes when the main process is closed
                 httpServer?.Stop();
             }
 
--- progetto pcto/progetto pcto/GlobeViewer/Classes/JavascriptIntegrationService.cs
+++ progetto pcto/progetto pcto/GlobeViewer/Classes/JavascriptIntegrationService.cs
@@ -15,24 +15,39 @@ namespace GlobeViewer.Classes
         private readonly JavascriptEventHandler eventHandler = default(JavascriptEventHandler);
         private readonly ChromiumWebBrowser browser = default(ChromiumWebBrowser);
 
+        /// <summary>
+        /// Initializes the Javascript integration service
+        /// </summary>
+        /// <param name="browser">Browser that integrates the Javascript integration service</param>
         public JavascriptIntegrationService(ChromiumWebBrowser browser)
         {
+            //Setting up Javascript event handling
             eventHandler = new JavascriptEventHandler();
             eventHandler.EventArrived += JavascriptEventHandlerEventArrived;
             this.browser = browser;
             browser.JavascriptObjectRepository.Register("boundEventHandler", eventHandler, true, BindingOptions.DefaultBinder);
         }
 
+        /// <summary>
+        /// Call a procedure (no return value)
+        /// </summary>
+        /// <param name="expression"></param>
         public void CallProcedure(string expression)
         {
             browser.ExecuteScriptAsync(expression);
         }
 
+        /// <summary>
+        /// Call a function (returns a "JavascriptResponse")
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <returns></returns>
         public JavascriptResponse CallFunction(string expression)
         {
             return GetDataAsync(expression).Result;
         }
 
+        //Identifies the event and calls its respective handler
         private void JavascriptEventHandlerEventArrived(string eventName, dynamic eventArgs)
         {
             if (eventName == "MarkerClick")
@@ -41,11 +56,13 @@ namespace GlobeViewer.Classes
             }
         }
 
+        //Calls Javascript function and waits to get a return value, asynchronously
         async Task<JavascriptResponse> GetDataAsync(string expression)
         {
             return await browser.EvaluateScriptAsync(expression);
         }
 
+        //Invokes the public "MarkerClicked"
         protected virtual void OnMarkerClicked(string location)
         {
             MarkerClicked?.Invoke(this, location);
