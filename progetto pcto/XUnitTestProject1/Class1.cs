262281114017b7aef9de41f004674f3323a399ff
Zaroual23 <62284738+Zaroual23@users.noreply.github.com>
2021-04-13 11:26:29 +0200

    agg

---
+++ progetto pcto/XUnitTestProject1/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest2
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
