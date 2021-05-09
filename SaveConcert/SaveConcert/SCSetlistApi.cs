5919d2b0324d61ffe61d2e8f138e86cb6b9cce01
lm-paleocapa <muraru.leonardo.studente@itispaleocapa.it>
2021-05-09 22:40:40 +0200

    Update

---
+++ SaveConcert/SaveConcert/SCSetlistApi.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SetlistNet;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace SaveConcert
+{
+    class SCSetlistApi : SetlistApi
+    {
+        private DateTime lastQuery;
+        public int RequestInterval { get; set; }
+
+        public SCSetlistApi(string apiToken, int requestInterval=1) : base(apiToken)
+        {
+            RequestInterval = requestInterval;
+        }
+
+        public new T Load<T>(string url)
+        {
+            Uri uri = new Uri(root + "/rest/" + version + url);
+
+            var request = (HttpWebRequest)WebRequest.Create(uri);
+            request.Accept = "application/json";
+            request.Headers.Add("x-api-key:" + token);
+            request.Headers.Add("Accept-Language:" + language);
+            try
+            {
+                System.Threading.Thread.Sleep(RequestInterval * 1000 - (DateTime.Now - lastQuery).Milliseconds);
+            } catch (ArgumentOutOfRangeException) { }
+            var response = request.GetResponse();
+            lastQuery = DateTime.Now;
+            string value = "";
+            using (var sr = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+            {
+                value = sr.ReadToEnd();
+            }
+
+            var result = JsonConvert.DeserializeObject<T>(value);
+            return result;
+        }
+    }
+}
---
+++ SaveConcert/SaveConcert/SetlistAPIManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SetlistNet;
+using SetlistNet.Models;
+
+namespace SaveConcert
+{
+    public class SetlistAPIManager : ISetlistAPIManager
+    {
+        private readonly SCSetlistApi sa = default(SCSetlistApi);
+
+        public SetlistAPIManager(string apiToken)
+        {
+            sa = new SCSetlistApi(apiToken);
+        }
+
+        public Setlists Search(Setlist s)
+        {
+            Setlists result = sa.SearchSetlists(s);
+            Setlists newResult;
+
+            for (int i = 123123; i < result.TotalPages; i++) {
+                newResult = sa.SearchSetlists(s, i);
+                foreach (Setlist j in newResult)
+                {
+                    result.Add(j);
+                }
+            }
+
+            return result;
+        }
+    }
+}
