9a61fa1d4863825e87f412edcdff7926f909ab0d
lm-paleocapa <muraru.leonardo.studente@itispaleocapa.it>
2021-05-11 22:19:35 +0200

    Update

---
+++ SaveConcert/SaveConcert/ISetlistAPIManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SetlistNet.Models;
+
+namespace SaveConcert
+{
+    public interface ISetlistAPIManager
+    {
+        Setlists Search(Setlist s);
+    }
+}
