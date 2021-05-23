fadba91b6f5dc610766be37e85f868458efed08b
Acevedo.Martin <60346328+Acevedo-Martin@users.noreply.github.com>
2021-05-23 19:42:28 +0200

    f

--- SaveConcert/SaveConcert/SetListView.Designer.cs
+++ SaveConcert/SaveConcert/SetListView.Designer.cs
@@ -31,14 +31,17 @@ namespace SaveConcert
         {
             this.panelForData = new System.Windows.Forms.Panel();
             this.labelArtist = new System.Windows.Forms.Label();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // panelForData
             // 
             this.panelForData.AutoScroll = true;
-            this.panelForData.Location = new System.Drawing.Point(12, 41);
+            this.panelForData.Location = new System.Drawing.Point(12, 65);
             this.panelForData.Name = "panelForData";
-            this.panelForData.Size = new System.Drawing.Size(776, 397);
+            this.panelForData.Size = new System.Drawing.Size(776, 373);
             this.panelForData.TabIndex = 0;
             // 
             // labelArtist
@@ -50,11 +53,45 @@ namespace SaveConcert
             this.labelArtist.TabIndex = 1;
             this.labelArtist.Text = "Artista: ";
             // 
+            // labelDate
+            // 
+            this.labelDate.AutoSize = true;
+            this.labelDate.Location = new System.Drawing.Point(12, 34);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(36, 13);
+            this.labelDate.TabIndex = 2;
+            this.labelDate.Text = "Data: ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(636, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Add Setlist to Spotify";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(636, 33);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Connect to Spotify";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // SetListView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelDate);
             this.Controls.Add(this.labelArtist);
             this.Controls.Add(this.panelForData);
             this.Name = "SetListView";
@@ -69,5 +106,8 @@ namespace SaveConcert
 
         private System.Windows.Forms.Panel panelForData;
         private System.Windows.Forms.Label labelArtist;
+        private System.Windows.Forms.Label labelDate;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- SaveConcert/SaveConcert/SetListView.cs
+++ SaveConcert/SaveConcert/SetListView.cs
@@ -8,6 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using SetlistNet.Models;
+using System.IO;
+using SpotifyAPI.Web;
+using SpotifyAPI;
+using SpotifyAPI.Web.Auth;
+using Newtonsoft.Json;
+using System.Security;
+using System.Security.Permissions;
 
 namespace SaveConcert
 {
@@ -21,15 +28,25 @@ namespace SaveConcert
             InitializeComponent();
 
             labelArtist.Text = "Artista: " + data.Artist.Name;
+            labelDate.Text = "Data: " + data.EventDate;
         }
+
+        List<string> names = new List<string>();
 
         private void SetListView_Load(object sender, EventArgs e)
         {
+
+            //if (File.Exists(SpotifyClass.CredentialsPath))
+            //{
+            //    button2.Visible = false;
+            //    client = new SpotifyClass();
+            //}
+
             int lastY = 0;
 
             foreach (var i in data.Sets)
             {
-                foreach(var j in i.Songs)
+                foreach (var j in i.Songs)
                 {
                     ListTileForSetListView item = new ListTileForSetListView(j.Name);
 
@@ -37,9 +54,192 @@ namespace SaveConcert
 
                     item.Location = new Point(item.Location.X, lastY);
                     lastY += 80;
+                    names.Add(j.Name);
                 }
-               
+
+            }
+
+
+            //if (names.Count == 0)
+            //{
+            //    button1.Enabled = false;
+            //    button2.Enabled = false;
+            //}
+        }
+
+        //SpotifyClass client = null;
+
+        //private async void button1_Click(object sender, EventArgs e)
+        //{
+
+
+
+
+        //    if (client != null)
+        //    {
+
+        //        var list = new List<SpotifyClass.Track>();
+
+
+        //        foreach (string i in this.names) list.Add(new SpotifyClass.Track(i, data.Artist.Name));
+
+
+        //        string a = data.Artist.Name + " " + data.TourName + " " + data.EventDate;
+
+        //        await client.createPlaylist(a, list);
+
+        //        button2.Visible = false;
+
+
+        //    }
+
+        //    else Console.WriteLine("Autenticati prima di aggiungere le canzoni");
+        //}
+
+        //private void button2_Click(object sender, EventArgs e)
+        {
+            if (client == null)
+            {
+                client = new SpotifyClass();
+
+            }
+
+        }
+
+    }
+    public class SpotifyClass
+    {
+        public const string CredentialsPath = "credentials";
+        private readonly string clientId = "fa3f6d3824f6436bbe0508b85c84b39c";
+        private readonly EmbedIOAuthServer _server = new EmbedIOAuthServer(new Uri("http://localhost:5000/callback"), 5000);
+
+        private SpotifyClient spotify;
+
+        public SpotifyClass()
+        {
+            if (File.Exists(CredentialsPath))
+
+
+            {
+                Start();
+            }
+            else
+            {
+                StartAuthentication();
+            }
+
+        }
+
+        private void Start()
+        {
+            var json = File.ReadAllText(CredentialsPath);
+            var token = JsonConvert.DeserializeObject<PKCETokenResponse>(json);
+
+            var authenticator = new PKCEAuthenticator(clientId, token);
+            authenticator.TokenRefreshed += (sender, tokens) => File.WriteAllText(CredentialsPath, JsonConvert.SerializeObject(tokens));
+
+            var config = SpotifyClientConfig.CreateDefault().WithAuthenticator(authenticator);
+
+            spotify = new SpotifyClient(config);
+
+            _server.Dispose();
+        }
+
+        private void StartAuthentication()
+        {
+            var (verifier, challenge) = PKCEUtil.GenerateCodes();
+
+            Task.WaitAll(_server.Start());
+
+            _server.AuthorizationCodeReceived += async (sender, response) =>
+            {
+                await _server.Stop();
+
+                PKCETokenResponse dataTask = await new OAuthClient().RequestToken(
+                  new PKCETokenRequest(clientId, response.Code, _server.BaseUri, verifier)
+                );
+
+                File.WriteAllText(CredentialsPath, JsonConvert.SerializeObject(dataTask));
+                File.SetAttributes(CredentialsPath, FileAttributes.ReadOnly);
+
+                Start();
+            };
+
+            var request = new LoginRequest(_server.BaseUri, clientId, LoginRequest.ResponseType.Code)
+            {
+                CodeChallenge = challenge,
+                CodeChallengeMethod = "S256",
+                Scope = new List<string> { SpotifyAPI.Web.Scopes.UgcImageUpload, SpotifyAPI.Web.Scopes.UserReadRecentlyPlayed, SpotifyAPI.Web.Scopes.UserReadPlaybackPosition, SpotifyAPI.Web.Scopes.UserTopRead, SpotifyAPI.Web.Scopes.UserLibraryRead, SpotifyAPI.Web.Scopes.UserLibraryModify, SpotifyAPI.Web.Scopes.PlaylistModifyPrivate, SpotifyAPI.Web.Scopes.PlaylistReadPrivate, SpotifyAPI.Web.Scopes.UserFollowRead, SpotifyAPI.Web.Scopes.PlaylistModifyPublic, SpotifyAPI.Web.Scopes.UserReadPrivate, SpotifyAPI.Web.Scopes.UserReadEmail, SpotifyAPI.Web.Scopes.AppRemoteControl, SpotifyAPI.Web.Scopes.Streaming, SpotifyAPI.Web.Scopes.UserReadCurrentlyPlaying, SpotifyAPI.Web.Scopes.UserModifyPlaybackState, SpotifyAPI.Web.Scopes.UserReadPlaybackState, SpotifyAPI.Web.Scopes.PlaylistReadCollaborative, SpotifyAPI.Web.Scopes.UserFollowModify }
+            };
+
+            Uri uri = request.ToUri();
+            try
+            {
+                BrowserUtil.Open(uri);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to open URL, manually open: {0}", uri);
             }
         }
+
+        public class Track
+        {
+            public string nome { get; }
+           
+            public string artista { get; }
+
+            public Track(string p_nome, string p_artista)
+            {
+                if (string.IsNullOrEmpty(p_nome)) throw new ArgumentNullException("Il nome non può essere vuoto");
+                if (string.IsNullOrEmpty(p_artista)) throw new ArgumentNullException("L'artista non può essere vuoto");
+
+                this.nome = p_nome.ToLower();
+                this.artista = p_artista.ToLower();
+            }
+        }
+
+        public async Task<string> findTrackUri(Track item)
+        {
+            bool ok = false;
+            string b = string.Empty;
+            while (!ok)
+            {
+                if (spotify != null)
+                {
+                    var a = await spotify.Search.Item(new SearchRequest(SearchRequest.Types.Track, $"artist:{item.artista} track:{item.nome}"));
+                    ok = true;
+                    b = a.Tracks.Items[0].Uri;
+                }
+
+            }
+            if (b != null)
+                return b;
+
+            return "";
+        }
+
+        public async Task createPlaylist(string name, List<Track> canzoni = null)
+        {
+            if (spotify == null) throw new Exception("Devi eseguire l'autenticazione prima di chiamare questo metodo");
+
+            if (string.IsNullOrEmpty(name)) name = "default";
+
+            var user = await spotify.UserProfile.Current();
+            var playlist = await spotify.Playlists.Create(user.Id, new PlaylistCreateRequest(name));
+
+            var songs = new List<string>();
+
+
+            foreach (var i in canzoni)
+            {
+                var id = await findTrackUri(i);
+                if (!string.IsNullOrEmpty(id)) songs.Add(id);
+            }
+
+            await spotify.Playlists.AddItems(playlist.Id, new PlaylistAddItemsRequest(songs));
+        }
     }
+
+
 }
